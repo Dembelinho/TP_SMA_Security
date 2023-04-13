cf6dac37783cfe3a6bb6d929043b68128755e61c
Dembelinho <dembeleayou86@gmail.com>
2023-04-13 11:58:34 +0000

    commit

--- src/Containers/AgentsContainer.java
+++ src/Containers/AgentsContainer.java
@@ -1,19 +1,30 @@
 package Containers;
 
+import AES.CryptoKeysGenerator;
 import jade.core.ProfileImpl;
 import jade.core.Runtime;
 import jade.wrapper.AgentContainer;
 import jade.wrapper.AgentController;
 import jade.wrapper.StaleProxyException;
 
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
 public class AgentsContainer {
-    public static void main(String[] args) throws StaleProxyException {
+    public static void main(String[] args) throws StaleProxyException, NoSuchAlgorithmException {
         Runtime instance = Runtime.instance();
         ProfileImpl profile=new ProfileImpl();
         profile.setParameter(ProfileImpl.MAIN_HOST,"localhost");
         AgentContainer agentContainer=instance.createAgentContainer(profile);
-        AgentController agentSeller=agentContainer.createNewAgent("server","agents.Server",new Object[]{});
-        AgentController agentBuyer=agentContainer.createNewAgent("client","agents.Client",new Object[]{});
+
+        KeyPair rsaKeys = CryptoKeysGenerator.generateRSAKeys();
+        PrivateKey privateKey = rsaKeys.getPrivate();
+        PublicKey publicKey = rsaKeys.getPublic();
+
+        AgentController agentSeller=agentContainer.createNewAgent("server","agents.Server",new Object[]{privateKey});
+        AgentController agentBuyer=agentContainer.createNewAgent("client","agents.Client",new Object[]{publicKey});
         agentSeller.start();
         agentBuyer.start();
 
--- src/agents/Client.java
+++ src/agents/Client.java
@@ -1,7 +1,37 @@
 package agents;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.lang.acl.ACLMessage;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.util.Arrays;
+import java.util.Base64;
 
 public class Client extends Agent {
+    @Override
+    protected void setup() {
+        PublicKey publicKey= (PublicKey) getArguments()[0];
+        String msg=" Voici le msg qu'on veut crypter";
+        try {
+            Cipher cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.ENCRYPT_MODE,publicKey);
+            byte[] cryptedMsg = cipher.doFinal(msg.getBytes());
+            String encodedMsg = Base64.getEncoder().encodeToString(cryptedMsg);
+            ACLMessage aclMessage=new ACLMessage(ACLMessage.INFORM);
+            aclMessage.addReceiver(new AID("server",AID.ISLOCALNAME));
+            aclMessage.setContent(encodedMsg);
+            send(aclMessage);
 
+            System.out.println(Arrays.toString(cryptedMsg));
+            System.out.println("---------------");
+            System.out.println(encodedMsg);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
